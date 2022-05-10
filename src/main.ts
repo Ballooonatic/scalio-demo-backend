f6cd5ad26f31b96049879ccafc1286158629dd51
Michael Imgrund <michael.imgrund@gmail.com>
2022-05-10 00:50:37 -0500

    add new API url to CORS settings

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:4200',
+        origin: [
+            'http://localhost:4200',
+            'https://telling-sandy-conchoraptor.glitch.me',
+        ],
         methods: 'GET',
     });
     await app.listen(3000);
